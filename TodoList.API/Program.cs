145522dd9006a8a73576fc3afab77268064a5b0d
Amirhossein Beheshti <a.beheshti@persici.co.uk>
2025-04-22 09:56:47 +0330

    feat: move files to correct project structure and prepare for API configuration

--- TodoList.API/Program.cs
+++ TodoList.API/Program.cs
@@ -1,13 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using TodoList.Application.Commands.TodoItems;
-using TodoList.Infrastructure.Data;
 using TodoList.Infrastructure.Extensions;
 using TodoList.Infrastructure.Mediator;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,7 +14,6 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddTodoListDbContext(builder.Configuration.GetConnectionString("DefaultConnection")!);
 builder.Services.AddRepositories();
 
-// Register MediatR
 builder.Services.AddMediator(typeof(CreateTodoItemCommand).Assembly);
 
 var app = builder.Build();
